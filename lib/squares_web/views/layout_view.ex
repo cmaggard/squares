8717c07d3219bb7300c8f1578eb51288a12d2d6f
Cody Maggard <cody.maggard@gmail.com>
2017-08-19 02:48:49 -0400

    Setting up webpack/postcss

--- lib/squares_web/views/layout_view.ex
+++ lib/squares_web/views/layout_view.ex
@@ -1,3 +1,19 @@
 defmodule SquaresWeb.LayoutView do
   use SquaresWeb, :view
+
+  def js_script_tag(file) do
+    ~s(<script src="#{_js_script_location(file, Mix.env)}"></script>)
+  end
+
+  def _js_script_location(file, :prod), do: "/js/#{file}"
+
+  def _js_script_location(file, _), do: "http://localhost:8080/js/#{file}"
+
+  def css_link_tag(file) do
+    if Mix.env == :prod do
+      ~s(<link rel="stylesheet" type="text/css" href="/css/#{file}" media="screen,projection" />)
+    else
+      ""
+    end
+  end
 end
